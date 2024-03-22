68aa018b0c908cd2b51c0d906d6e8ac7ed660d88
Xiaosu Lyu <lyuxiaosu@gwu.edu>
2024-03-22 14:54:31 -0600

    update start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -1,1 +1,3 @@
-PATH=./bin:$PATH ./bin/executor_manager -c executor_manager.json --device-database server_devices.json --skip-resource-manager --max-funcs 6 
+#PATH=./bin:$PATH gdb -ex run --args \
+PATH=./bin:$PATH \
+./bin/executor_manager -c executor_manager.json --device-database server_devices.json --skip-resource-manager --max-funcs 100 
