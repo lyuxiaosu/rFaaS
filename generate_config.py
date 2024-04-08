00eb0440df079251c5b84c3c7650ad783be14224
Xiaosu Lyu <lyuxiaosu@gwu.edu>
2024-04-08 14:32:54 -0600

    update generate_config.py

--- generate_config.py
+++ generate_config.py
@@ -2,26 +2,42 @@ import sys
 import os
 
 
-def generate_config(loop_count):
-    loop_count = int(loop_count)
+def generate_config(type1_con, type2_con, type1_rps, type2_rps, type1_param, type2_param):
     function = "./examples/libfibonacci.so"
     name = "fibonacci"
     type1 = "1"
     type2 = "2"
-    para1 = "1"
-    para2 = "32"
 
     config = []
     config.append(f"--config multi_functions.json")
     config.append("--device-database client_devices.json")
-    config.append("--names " + ",".join([name]*loop_count))
-    config.append("--functions " + ",".join([function]*loop_count))
-    parameter1 = [para1] * (loop_count // 2)
-    parameter2 = [para2] * (loop_count // 2)
-    config.append("--req-parameters " + ",".join(parameter1) + "," + ",".join(parameter2))
-    req_type1 = [type1] * (loop_count // 2)
-    req_type2 = [type2] * (loop_count // 2)
-    config.append("--req-type " + ",".join(req_type1) + "," + ",".join(req_type2))
+    config.append("--names " + ",".join([name]*(type1_con + type2_con)))
+    config.append("--functions " + ",".join([function]*(type1_con + type2_con)))
+  
+    if type1_con == 0:
+        rps2 = [type2_rps] * type2_con
+        config.append("--rps " + ",".join(rps2))
+        parameter2 = [type2_param] * type2_con
+        config.append("--req-parameters " + ",".join(parameter2))
+        req_type2 = [type2] * type2_con
+        config.append("--req-type " + ",".join(req_type2))
+    elif type2_con == 0:
+        rps1 = [type1_rps] * type1_con
+        config.append("--rps " + ",".join(rps1))
+        parameter1 = [type1_param] * type1_con
+        config.append("--req-parameters " + ",".join(parameter1))
+        req_type1 = [type1] * type1_con
+        config.append("--req-type " + ",".join(req_type1))
+    else:
+        rps1 = [type1_rps] * type1_con
+        rps2 = [type2_rps] * type2_con
+        parameter1 = [type1_param] * type1_con
+        parameter2 = [type2_param] * type2_con
+        config.append("--rps " + ",".join(rps1) + "," + ",".join(rps2))
+        config.append("--req-parameters " + ",".join(parameter1) + "," + ",".join(parameter2))
+        req_type1 = [type1] * type1_con
+        req_type2 = [type2] * type2_con
+        config.append("--req-type " + ",".join(req_type1) + "," + ",".join(req_type2))
     config.append("--executors-database executors_database.json")
     config.append("--test-ms 10000")
     config.append("--input-size 1")
@@ -29,13 +45,18 @@ def generate_config(loop_count):
     config.append(f"--output-stats client.log")
     return "\n".join(config)
 
-if len(sys.argv) != 2:
-    print("Usage: <loop_count>")
+if len(sys.argv) != 7:
+    print("Usage: <type1_concurrency> <type2_concurrency> <type1_rps> <type2_rps> <type1_param> <type2_param>")
     sys.exit(1)
 
-loop_count = sys.argv[1]
+type1_con = int(sys.argv[1])
+type2_con = int(sys.argv[2])
+type1_rps = sys.argv[3]
+type2_rps = sys.argv[4]
+type1_param = sys.argv[5]
+type2_param = sys.argv[6]
 
-config_content = generate_config(loop_count)
+config_content = generate_config(type1_con, type2_con, type1_rps, type2_rps, type1_param, type2_param)
 with open("client_config", "w") as f:
     f.write(config_content)
 
