dfdd0b41a7adb5a2bfbe4a0773e4a1c31e7eaf19
Xiaosu Lyu <lyuxiaosu@gwu.edu>
2024-04-20 11:59:42 -0600

    change kill_rfaas.sh

--- kill_rfaas.sh
+++ kill_rfaas.sh
@@ -4,3 +4,6 @@ pid=`ps -ef|grep  "executor_manager"|grep -v grep |awk '{print $2}'`
 echo $pid
 sudo kill -9 $pid
 
+pid=`ps -ef|grep  "executor"|grep -v grep |awk '{print $2}'`
+sudo kill -9 $pid
+
