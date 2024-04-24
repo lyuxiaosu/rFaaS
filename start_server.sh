d416dd6c176ab0ceede179237974a96245284caf
Xiaosu Lyu <lyuxiaosu@gwu.edu>
2024-04-24 11:42:36 -0600

    add ulimit -n 65535 to start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -1,3 +1,4 @@
+ulimit -n 65535
 #PATH=./bin:$PATH gdb -ex run --args \
 PATH=./bin:$PATH \
 ./bin/executor_manager -c executor_manager.json --device-database server_devices.json --skip-resource-manager --max-funcs 100 
