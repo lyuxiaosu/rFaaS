0ac9a3be87f08b7b222703e316c25d537e888b94
Xiaosu Lyu <lyuxiaosu@gwu.edu>
2024-05-29 16:00:18 -0600

    rename examples/functions to examples/empty

--- examples/empty.cpp
+++ examples/empty.cpp
@@ -3,8 +3,8 @@
 
 extern "C" uint32_t empty(void* args, uint32_t size, void* res)
 {
-  int* src = static_cast<int*>(args), *dest = static_cast<int*>(res);
-  *dest = *src;
+  //int* src = static_cast<int*>(args), *dest = static_cast<int*>(res);
+  //*dest = *src;
   return size;
 }
 
